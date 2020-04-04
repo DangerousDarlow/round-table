89357b958603ed36ff7590964d4cbe8da18dc1fe
Nick Darlow <dangerous.darlow@gmail.com>
2020-04-04 19:12:05 +0100

    updated good vs evil players in 9 player game to match Avalon instruction manual

--- src/main/kotlin/com/noicesoftware/roundtable/dealing/DeckBuilder.kt
+++ src/main/kotlin/com/noicesoftware/roundtable/dealing/DeckBuilder.kt
@@ -6,9 +6,16 @@ import org.springframework.stereotype.Component
 @Component
 class DeckBuilder {
     fun build(players: Int): Map<Character, Int> {
-        if (players < 5 || players > 10) throw Exception("Player count ($players) must be between 5 and 10 inclusive")
+        val evil = when (players) {
+            5 -> 2
+            6 -> 2
+            7 -> 3
+            8 -> 3
+            9 -> 3
+            10 -> 4
+            else -> throw Exception("Player count ($players) must be between 5 and 10 inclusive")
+        }
 
-        val evil = (players - 1) / 2
         val good = players - evil
         return mapOf(
                 Character.Servant to good - 1,
--- src/test/kotlin/com/noicesoftware/roundtable/DeckBuilderTests.kt
+++ src/test/kotlin/com/noicesoftware/roundtable/DeckBuilderTests.kt
@@ -28,6 +28,38 @@ class DeckBuilderTests {
     }
 
     @Test
+    fun build_seven_card_deck() {
+        val deck = deckBuilder.build(7)
+        assertThat(deck[Character.Servant]).isEqualTo(3)
+        assertThat(deck[Character.Minion]).isEqualTo(3)
+        assertThat(deck[Character.Merlin]).isEqualTo(1)
+    }
+
+    @Test
+    fun build_eight_card_deck() {
+        val deck = deckBuilder.build(8)
+        assertThat(deck[Character.Servant]).isEqualTo(4)
+        assertThat(deck[Character.Minion]).isEqualTo(3)
+        assertThat(deck[Character.Merlin]).isEqualTo(1)
+    }
+
+    @Test
+    fun build_nine_card_deck() {
+        val deck = deckBuilder.build(9)
+        assertThat(deck[Character.Servant]).isEqualTo(5)
+        assertThat(deck[Character.Minion]).isEqualTo(3)
+        assertThat(deck[Character.Merlin]).isEqualTo(1)
+    }
+
+    @Test
+    fun build_ten_card_deck() {
+        val deck = deckBuilder.build(10)
+        assertThat(deck[Character.Servant]).isEqualTo(5)
+        assertThat(deck[Character.Minion]).isEqualTo(4)
+        assertThat(deck[Character.Merlin]).isEqualTo(1)
+    }
+
+    @Test
     fun cannot_build_two_card_deck() {
         assertThat { deckBuilder.build(2) }.isFailure().messageContains("between 5 and 10")
     }
